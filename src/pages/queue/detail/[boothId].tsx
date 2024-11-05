4fdb3d7abbd037e20ef83b4adcb43d30a854b063
soohyun <s2ksh_77@naver.com>
2024-11-06 02:27:44 +0900

    feat: boothId SSG 적용

--- src/pages/queue/detail/[boothId].tsx
+++ src/pages/queue/detail/[boothId].tsx
@@ -1,20 +1,22 @@
 import Header from "@/components/layout/header";
 import { useRouter } from "next/router";
-import { useBooth } from "@/hooks";
 import { useTranslation } from "react-i18next";
 import BoothInfo from "@/components/Booth/BoothInfo";
 import BoothForm from "@/components/InputForm/BoothForm";
 import { useAddReservation } from "@/hooks/useReservation";
 import { TRANSLATE_META } from "@/common/constants";
-import { Suspense } from "react";
-import Loader from "@/components/Loader/Loader";
-import { BoothDetailItemType, ReservationSubmitType } from "../types";
 
-export default function Detail() {
+import {
+  BoothDetailItemType,
+  BoothItemType,
+  ReservationSubmitType,
+} from "../types";
+import { boothRepository } from "@/stores/repository/BoothRepository";
+
+export default function Detail({ data }: { data: BoothDetailItemType }) {
   const router = useRouter();
   const { t } = useTranslation(TRANSLATE_META.DETAIL);
   const { boothId } = router.query;
-  const { data, isLoading } = useBooth(boothId as string) || {};
   const { mutateAsync } = useAddReservation();
 
   if (!boothId) return;
@@ -33,21 +35,38 @@ export default function Detail() {
   };
 
   return (
-    <Suspense fallback={<Loader />}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className="container -waiting_detail">
-          <Header />
-          <div className="content">
-            <BoothInfo data={data as BoothDetailItemType} />
-            <BoothForm
-              data={data as BoothDetailItemType}
-              onSubmit={handleSubmit}
-            />
-          </div>
-        </div>
-      )}
-    </Suspense>
+    <div className="container -waiting_detail">
+      <Header />
+      <div className="content">
+        <BoothInfo data={data as BoothDetailItemType} />
+        <BoothForm data={data as BoothDetailItemType} onSubmit={handleSubmit} />
+      </div>
+    </div>
   );
+}
+
+export async function getStaticPaths() {
+  const booths = await boothRepository.getBooths();
+
+  const paths = booths.map((booth: BoothItemType) => ({
+    params: { boothId: booth.id },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context: { params: { boothId: string } }) {
+  const { params } = context;
+  const boothId = params.boothId;
+
+  const data = await boothRepository.getBoothById(boothId);
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
