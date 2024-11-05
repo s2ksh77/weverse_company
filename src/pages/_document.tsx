b84bdcc622c0fd792f567468428d14e5dce99db6
soohyun <s2ksh_77@naver.com>
2024-11-06 02:35:00 +0900

    feat: meta data 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content={"비즈서비스_줄서기 부스"} />
+        <meta property="og:title" content="줄서기 부스" />
+        <meta property="og:site_name" content="줄서기 부스" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://localhost:3000/" />
+        <meta property="og:description" content={"비즈서비스_줄서기 부스"} />
+        <meta property="og:locale" content="ko" />
+      </Head>
       <body>
         <Main />
         <NextScript />
